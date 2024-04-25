994e04452c731bd4b1b32069afd34e0267a8cc30
EmilseBilse <bandittenxd@gmail.com>
2024-04-25 17:43:49 +0200

    better icon

--- src/components/invitesNotification.vue
+++ src/components/invitesNotification.vue
@@ -3,13 +3,12 @@
 </script>
 
 <template>
-    <article>
-        <section class="flex bg-blue-600 aspect-square p-1 rounded-md relative">
-            <i class="bi bi-bell-fill text-2xl flex justify-center items-center text-white"></i>
-            <span class="absolute flex justify-center items-center text-black font-bold text-xs bottom-0 right-0 bg-red-600 rounded-full aspect-square">
-                9
+    <article class="p-4">
+        <section class="flex justify-center items-center bg-blue-600 w-10 h-10 rounded-full relative cursor-pointer">
+            <i class="bi bi-bell-fill text-3xl text-white"></i>
+            <span class="flex justify-center items-center text-white font-bold text-xs w-5 h-5 bg-red-600 rounded-full absolute bottom-0 right-0 transform translate-x-1/5 -translate-y-1/5 pointer-events-none">
+                9+
             </span>
         </section>
     </article>
-
 </template>
